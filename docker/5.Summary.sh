b3d7b4682b97e9a724cc274371752448fcc04453
Jose Hidalgo <c0d5x@protonmail.com>
2020-06-30 08:59:59 -0600

    more commands

--- docker/5.Summary.sh
+++ docker/5.Summary.sh
@@ -31,6 +31,13 @@ docker exec "container" cat /etc/passwd
 docker stop "container"
 docker kill "container"
 
+# Logs
+docker logs "container"
+docker logs -f "container"
+
+# Stats
+docker stats "container"
+
 # Create a network
 docker network create mynetwork
 
@@ -55,9 +62,29 @@ tar -C container-fs-root -xvf container-fs.tar
 
 # Clean up
 docker system prune
+docker rm $(docker ps -a -q)
+docker rmi $(docker images -q)
+docker rm $(docker ps -a -q --filter 'exited!=0')
 
 # Disk space
 docker system df
 
 # Info
+docker version
 docker system info
+
+# Event monitoring
+docker events
+-> open another terminal
+docker run ....
+<- close/stop the new container
+
+# cAdvisor, web interface to monitor cpu and memory, open http://localhost:8080
+docker run \
+  --volume=/:/rootfs:ro \
+  --volume=/var/run:/var/run:rw \
+  --volume=/sys:/sys:ro \
+  --volume=/var/lib/docker/:/var/lib/docker:ro \
+  --publish=8080:8080 \
+  --name=cadvisor \
+  google/cadvisor:latest
