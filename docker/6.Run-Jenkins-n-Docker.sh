7204bea151b64dea27174b3f7bdbaad20c2df065
Jose Hidalgo <jose.hidalgo@xogito.com>
2020-06-30 01:24:31 -0600

    alpha

--- docker/6.Run-Jenkins-n-Docker.sh
+++ docker/6.Run-Jenkins-n-Docker.sh
@@ -13,9 +13,9 @@ docker container run --name jenkins-docker --rm --detach \
 docker container run --name jenkins-tutorial --rm --detach \
   --network jenkins --env DOCKER_HOST=tcp://docker:2376 \
   --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 \
-  --volume jenkins-data:/var/jenkins_home \ 
+  --volume jenkins-data:/var/jenkins_home \
   --volume jenkins-docker-certs:/certs/client:ro \
-  --volume "$HOME":/home \ 
+  --volume "$HOME":/home \
   --publish 8080:8080 jenkinsci/blueocean
 
 # show the admin password to start jenkins
