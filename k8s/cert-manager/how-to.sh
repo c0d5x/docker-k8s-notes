e64e77c88faed52e9a9f9fa093b2ef1f28b9fca4
Jose Hidalgo <jose.hidalgo@xogito.com>
2020-08-31 21:17:48 -0600

    cert manager

---
+++ k8s/cert-manager/how-to.sh
@@ -0,0 +1,22 @@
+
+# install CRDs
+kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v0.16.1/cert-manager.yaml
+
+# create namespace
+kubectl create namespace cert-manager
+
+# more CRDs
+kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v0.16.1/cert-manager.crds.yaml
+
+# check pods were deployed
+kubectl get pods --namespace cert-manager
+
+
+kubectl apply -f test-certs.yml
+
+# check if test cert worked
+kubectl describe certificate -n cert-manager-test
+
+kubectl delete -f test-certs.yml
+
+
